3cc9c5af4044d7a9665c42e49fbb7befbca07747
CodeAndJesus <codeandjesus@gmail.com>
2020-06-15 20:21:29 -0500

    HolaMundo commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Github
+                    Hola Mundo
                 </div>
 
                 <div class="links">
